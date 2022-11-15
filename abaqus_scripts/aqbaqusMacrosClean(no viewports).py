cf8ff387992f53dae967ce7e7d873fa9a70d7e7d
Snow Queen <71298338+snowqueen2000@users.noreply.github.com>
2022-11-15 11:21:55 -0700

    added odb output and odb to csv part cleaned up viewports and renamed m

--- abaqus_scripts/aqbaqusMacrosClean(no viewports).py
+++ abaqus_scripts/aqbaqusMacrosClean(no viewports).py
@@ -9,9 +9,10 @@ def Macro1():
     import mesh
     import displayGroupOdbToolset as dgo
 
-   
+    m = mdb.models['Model-1']
+    
     #GEOMETRY
-    s = mdb.models['Model-1'].ConstrainedSketch(name='__profile__', sheetSize=5.0)
+    s = m.ConstrainedSketch(name='__profile__', sheetSize=5.0)
     g, v, d, c = s.geometry, s.vertices, s.dimensions, s.constraints
     s.setPrimaryObject(option=STANDALONE)
      
@@ -19,23 +20,21 @@ def Macro1():
     s.CircleByCenterPerimeter(center=(0.1, 0.1), point1=(0.1, 0.1125))
     
     #MAKE PART
-    p = mdb.models['Model-1'].Part(name='PlateWithHole', 
+    p = m.Part(name='PlateWithHole', 
         dimensionality=TWO_D_PLANAR, type=DEFORMABLE_BODY)
     
     p.BaseShell(sketch=s)
     s.unsetPrimaryObject()
 
-    del mdb.models['Model-1'].sketches['__profile__']
+    del m.sketches['__profile__']
 
     #SAVE
     mdb.saveAs(pathName='X:/PlateWithHole')
     
     #MAKE MATERIAL
-    mdb.models['Model-1'].Material(name='Steel')
-    mdb.models['Model-1'].materials['Steel'].Elastic(table=((200000000000.0, 0.32), 
-        ))
-    mdb.models['Model-1'].HomogeneousSolidSection(name='PlateSection', 
-        material='Steel', thickness=0.001)
+    m.Material(name='Steel')
+    m.materials['Steel'].Elastic(table=((200000000000.0, 0.32), ))
+    m.HomogeneousSolidSection(name='PlateSection', material='Steel', thickness=0.001)
 
     #SECTION ASSIGNMENT
     f = p.faces
@@ -46,38 +45,34 @@ def Macro1():
         offsetType=MIDDLE_SURFACE, offsetField='', 
         thicknessAssignment=FROM_SECTION)
     
-    a = mdb.models['Model-1'].rootAssembly
+    a = m.rootAssembly
     
     #CREATE INSTANCE
     a.DatumCsysByDefault(CARTESIAN)
     a.Instance(name='PlateWithHole-1', part=p, dependent=ON)
     
     #STEPS
-    mdb.models['Model-1'].StaticStep(name='Step-1', previous='Initial')
+    m.StaticStep(name='Step-1', previous='Initial')
     
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step='Step-1')
-    
-    mdb.models['Model-1'].steps['Step-1'].setValues(description='')
+    m.steps['Step-1'].setValues(description='')
     
     e1 = a.instances['PlateWithHole-1'].edges
     edges1 = e1.getSequenceFromMask(mask=('[#10 ]', ), )
     region = a.Set(edges=edges1, name='Set-1')
     
-    mdb.models['Model-1'].EncastreBC(name='FixedLeftEdge', 
-        createStepName='Initial', region=region, localCsys=None)
+    m.EncastreBC(name='FixedLeftEdge', createStepName='Initial', region=region, localCsys=None)
     
-    session.viewports['Viewport: 1'].assemblyDisplay.setValues(step='Step-1')
     s1 = a.instances['PlateWithHole-1'].edges
     side1Edges1 = s1.getSequenceFromMask(mask=('[#4 ]', ), )
     
     region = a.Surface(side1Edges=side1Edges1, name='Surf-1')
     
-    mdb.models['Model-1'].Pressure(name='PressureRightEdge', 
+    m.Pressure(name='PressureRightEdge', 
         createStepName='Step-1', region=region, distributionType=UNIFORM, 
         field='', magnitude=-1000000.0, amplitude=UNSET)
     
     #MESH PART    
-    p1 = mdb.models['Model-1'].parts['PlateWithHole']
+    p1 = m.parts['PlateWithHole']
     
     elemType1 = mesh.ElemType(elemCode=CPS4R, elemLibrary=STANDARD, 
         secondOrderAccuracy=OFF, hourglassControl=DEFAULT, 
@@ -92,6 +87,9 @@ def Macro1():
     p.generateMesh()
     a.regenerate()
     
+    
+    
+    # this will be in it's own submit job script
     #CREATE JOB
     mdb.Job(name='Job-1', model='Model-1', description='', type=ANALYSIS, 
         atTime=None, waitMinutes=0, waitHours=0, queue=None, memory=90, 
@@ -103,3 +101,24 @@ def Macro1():
     
     #SUBMIT
     mdb.jobs['Job-1'].submit(consistencyChecking=OFF)
+    
+    # Get ODB
+    session.mdbData.summary()
+    o3 = session.openOdb(name='C:/temp/Approx005V1.odb')
+    a = m.rootAssembly
+    #a = m.rootAssembly
+    #o3 = session.openOdb(name='C:/temp/Approx005V1.odb')
+    #session.mdbData.summary()
+    odb = session.odbs['C:/temp/Approx005V1.odb']
+    
+    # this will be in ti's own get CSV script
+    # Convert ODB to CSV for readable data
+    session.fieldReportOptions.setValues(reportFormat=COMMA_SEPARATED_VALUES)
+    session.writeFieldReport(fileName='X:/Desktop/DataPlateWHoleV1.csv', append=ON, 
+        sortItem='Node Label', odb=odb, step=0, frame=1, outputPosition=NODAL, 
+        variable=(('S', INTEGRATION_POINT, ((INVARIANT, 'Mises'), (INVARIANT, 
+        'Max. In-Plane Principal'), (INVARIANT, 
+        'Max. In-Plane Principal (Abs)'), (INVARIANT, 'Max. Principal'), (
+        INVARIANT, 'Max. Principal (Abs)'), )), ), stepFrame=SPECIFY)
+    
+    
