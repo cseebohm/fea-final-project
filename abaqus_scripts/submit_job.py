40b348b4f8eaaeb105d8bb2e33340627252a24e1
cseebohm <clarissaseebohm@gmail.com>
2022-11-15 17:09:16 -0700

    added needed outputs to for output_data

--- abaqus_scripts/submit_job.py
+++ abaqus_scripts/submit_job.py
@@ -1,6 +1,11 @@
 """
 this script defines a function to submit a job from an existing model, it does not loop
+??it should output an odb file??
 
 @version 11-15-2022
 @author Clarissa Seebohm and Audrey Pohl
 """
+
+def submit_job():
+    
+    return o1, odb
