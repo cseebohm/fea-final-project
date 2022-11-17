02756252c2f2a0b3247187ca5d5b3ff98680c248
cseebohm <clarissaseebohm@gmail.com>
2022-11-17 11:13:27 -0700

    added V4_phase_one.py

---
+++ abaqus_scripts/V4_phase_one.py
@@ -0,0 +1,6 @@
+"""
+this script is to generate training data for phase one: varying the radius of a hole located in the center of the plate
+
+@version 11-17-2022
+@author Clarissa Seebohm and Audrey Pohl
+"""
