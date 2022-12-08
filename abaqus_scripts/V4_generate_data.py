ae8f609508a9c4eac1174e3cfea0bbdf49cd3e2b
cseebohm <clarissaseebohm@gmail.com>
2022-12-08 12:02:30 -0700

    rename V4

--- abaqus_scripts/V4_generate_data.py
+++ abaqus_scripts/V4_generate_data.py
@@ -1,5 +1,5 @@
 """
-this script is to generate training data for phase one: varying the radius of a hole located in the center of the plate
+this script is to generate training data for varying the radius of a hole located in the center of the plate
 
 @version 11-17-2022
 @author Clarissa Seebohm and Audrey Pohl
